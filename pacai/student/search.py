77ca1c4e1b2f46a1fa58b88f6e03c991bf5900fb
Andrew Nguyen <anguy424@ucsc.edu>
2024-10-02 00:49:46 -0700

    TODO: Implement move stack

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -20,16 +20,13 @@ def depthFirstSearch(problem):
     """
 
     # *** Your Code Here ***
-    
-    # Initialize node, frontier stack, and reached set
-    node = problem.startingState()
-    moves = []  # List of moves to return to agent
-    if problem.isGoal(node):
-        return moves
+    # Initialize current state, frontier stack, and reached set
+    # TODO: Implement move stack
+    current_state = problem.startingState()
+    moves = Stack()
     frontier = Stack()
-    moveset = Stack()
-    frontier.push(node)
     reached = set()
+    frontier.push(current_state)
     
 
     # While frontier is not empty:
@@ -37,14 +34,13 @@ def depthFirstSearch(problem):
     # 2. Check if the node is the goal state: if it is, return it.
     # 3. If not, add the node to the set of explored nodes and push all unvisited successors onto the frontier stack
     while not frontier.isEmpty():
-        node = frontier.pop()
-        if problem.isGoal(node):
+        current_state = frontier.pop()
+        if problem.isGoal(current_state):
             return moves
-        reached.add(node)
-        successors = problem.successorStates(node)
+        reached.add(current_state)
+        successors = problem.successorStates(current_state)
         for state in successors:
             temp_node = state[0]
-            temp_move = state[1]
             if temp_node not in frontier.list and temp_node not in reached:
                 frontier.push(temp_node)
     return None
