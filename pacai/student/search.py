cbbe7b7769ee38554eb3b96ee6a8a1da7c50f43b
Andrew N <anguy424@ucsc.edu>
2024-10-02 14:24:50 -0700

    Completed BFS for pacman ai. TODO: Change DFS and BFS to be more general to other problems

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -2,6 +2,7 @@
 In this file, you will implement generic search algorithms which are called by Pacman agents.
 """
 from pacai.util.stack import Stack
+from pacai.util.queue import Queue
 from pacai.core.actions import Actions
 
 class Node:
@@ -25,7 +26,6 @@ def depthFirstSearch(problem):
     """
     # *** Your Code Here ***
     # Initialize current state, frontier stack, and reached set
-    # TODO: Implement move stack
     node = Node(problem.startingState())
     frontier = Stack()
     reached = set()
@@ -53,8 +53,6 @@ def depthFirstSearch(problem):
 
             return moves
 
-
-            
         reached.add(node.state)
         successors = problem.successorStates(node.state)
         for state in successors:
@@ -74,9 +72,45 @@ def breadthFirstSearch(problem):
     """
     Search the shallowest nodes in the search tree first. [p 81]
     """
+    
 
     # *** Your Code Here ***
-    raise NotImplementedError()
+    # Initialize current state, frontier queue, and reached set
+    node = Node(problem.startingState())
+    frontier = Queue()
+    reached = set()
+    frontier.push(node)
+    
+
+    # While frontier is not empty:
+    # 1. Pop frontier stack and set node to popped element
+    # 2. Check if the node is the goal state: if it is, return it.
+    # 3. If not, add the node to the set of explored nodes and push all unvisited successors onto the frontier stack
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        if problem.isGoal(node.state):
+            moves = []  # Array to return
+            parent = node.parent
+            while parent is not None:
+                # Calculate vector direction from parent node to current node
+                # Convert vector to direction, and prepend it to move list
+                # Set node to parent, and parent to node.parent
+                vector = (node.state[0] - parent.state[0], node.state[1] - parent.state[1])
+                action = Actions.vectorToDirection(vector)
+                moves.insert(0, action)
+                node = parent
+                parent = node.parent
+
+            return moves
+
+        reached.add(node.state)
+        successors = problem.successorStates(node.state)
+        for state in successors:
+            temp_state = state[0]
+            if temp_state not in frontier.list and temp_state not in reached:
+                temp_node = Node(temp_state, node)
+                frontier.push(temp_node)
+    return None
 
 def uniformCostSearch(problem):
     """
