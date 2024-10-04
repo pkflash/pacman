39ac69dd19867075966b1484edbb2105d19ad7dd
Andrew N <anguy424@ucsc.edu>
2024-10-04 11:43:40 -0700

    Deleted UCS code, must adjust Node class

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -7,6 +7,8 @@ from pacai.util.priorityQueue import PriorityQueue
 from pacai.core.actions import Actions
 
 class Node:
+
+    # TODO: Implement path cost in Node class
     def __init__(self, state, action=None, parent=None):
         self.state = state
         self.parent = parent
@@ -14,6 +16,8 @@ class Node:
     
     def __eq__(self, otherNode):
         return self.state == otherNode.state
+    def __lt__(self, otherNode):
+        return self.state < otherNode.state
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first [p 85].
@@ -55,6 +59,7 @@ def depthFirstSearch(problem):
 
         reached.add(node.state)
         successors = problem.successorStates(node.state)
+        print(successors)
         for state in successors:
             temp_state = state[0]
             temp_action = state[1]
@@ -119,36 +124,7 @@ def uniformCostSearch(problem):
     node = Node(problem.startingState())
     frontier = PriorityQueue()
     reached = set()
-    frontier.push(node, 0)
 
-    # While frontier is not empty:
-    # 1. Pop frontier queue and set node to popped element
-    # 2. Check if the node is the goal state: if it is, return it.
-    # 3. If not, add the node to the set of explored nodes and push all unvisited successors onto the frontier queue
-    while not frontier.isEmpty():
-        node = frontier.pop()
-        if problem.isGoal(node.state):
-            moves = []  # Array to return
-            parent = node.parent
-            while parent is not None:
-                moves.insert(0, node.action)
-                node = parent
-                parent = node.parent
-
-            return moves
-
-        reached.add(node.state)
-        successors = problem.successorStates(node.state)
-        for state in successors:
-            temp_state = state[0]
-            temp_action = state[1]
-            temp_priority = state[2]
-            temp_node = Node(temp_state, temp_action, node)
-
-            # TODO: Adjust way to check elements in frontier
-            if temp_node not in frontier.list and temp_state not in reached:
-                frontier.push(temp_node, temp_priority)
-    return None
 
 
 
